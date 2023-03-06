bbd74a3c0cb42d7e54c72afcb8bfaf9fe0fc7ab6
ayushlohmod <ayushlohmod@gmail.comd>
2023-03-06 20:00:46 +0530

    added pin section

--- sharewall_frontend/src/components/CreatePin.jsx
+++ sharewall_frontend/src/components/CreatePin.jsx
@@ -9,7 +9,7 @@ import { categories } from '../utils/data'
 
 
 
-const CreatePin = ( { user }) => {
+const CreatePin = ({ user }) => {
   const [title, setTitle] = useState('')
   const [about, setAbout] = useState('')
   const [destination, setDestination] = useState('')
@@ -22,14 +22,14 @@ const CreatePin = ( { user }) => {
   const navigate = useNavigate();
 
   const uploadImage = (e) =>{
-    const {type, name} = e.target.file[0];
+    const {type, name} = e.target.files[0];
 
     if(type === 'image/png' || type === 'image/jpeg' || type === "image/gif" || type === "image/tiff"){
       setWrongImagetype(false);
       setLoading(true);
 
       client.assets
-      .upload('image', e.target.file[0], {contentType: type, filename: name })
+      .upload('image', e.target.files[0], {contentType: type, filename: name })
       .then((document) =>{
         setImageAsset(document);
         setLoading(false);
@@ -42,10 +42,39 @@ const CreatePin = ( { user }) => {
     }else{
       setWrongImagetype(true);
     }
-
-
   }
-
+const savePin = () => {
+  if(title && about && destination && imageAsset?._id && category){
+    const doc = {
+      _type:'pin',
+      title,
+      about,
+      destination,
+      image:{
+        _type:'image',
+        asset:{
+          _type:'reference',
+          _ref: imageAsset?._id
+        }
+      },
+      userId: user,
+      postedBy:{
+        _type:'postedBy',
+        _ref: user,
+        },
+        category,
+      }
+      client.create(doc)
+      .then(()=>{
+        navigate('/')
+      })
+  } else{
+    setFields(true);
+  
+    setTimeout(()=> setFields(false), 2000)
+  
+  }
+}
 
 
   return (
@@ -97,12 +126,91 @@ const CreatePin = ( { user }) => {
                 className='absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor- pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out'
                 onClick={() => setImageAsset(null)}
               >
+                <MdDelete />
 
 
               </button>
             </div>
           )}
         </div>
+
+
+      </div>
+      <div className=' flex flex-1 flex-col gap-6 lg:pl-5-5 mt-5 w-full'>
+            <input 
+              type='text'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder='Add your title here'
+              className='outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2'
+              
+
+
+            />
+            {user && (
+              <div className='flex gap-2 my-2 items-center bg-white rounded-lg'>
+                <img 
+                  src={user.image}
+                  className='w-10 h-10 rounded-full'
+                  alt='user-profile'
+
+                />
+                <p className='font-bold'>{user.userName}</p>
+
+              </div>
+            )}
+            <input 
+              type='text'
+              value={about}
+              onChange={(e) => setAbout(e.target.value)}
+              placeholder='What is your pin about'
+              className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+              
+            />
+
+            <input 
+              type='text'
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              placeholder='Add a destination link'
+              className='outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2'
+            />
+            <div className='flex flex-col'>
+              <div>
+
+                <p className='mb-2 font-semibold text-lg sm:text-xl'> Choose Pin Category
+                </p>
+                <select 
+                  onChange={(e) => setCategory(e.target.value)}
+                  className='outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer'                
+                
+                >
+                <option value='other' className='bg-white'> Select category </option>
+                {categories.map((category)=>(
+                  <option className='text-base border-0 outline-none capitalize bg-white text-black' value={category.name} >
+
+                    {category.name}
+                  </option>
+                ))}
+                
+
+
+                </select>
+              </div>
+              <div className='flex justify-end items-end mt-5'>
+                  <button
+                    type='button'
+                    onClick={savePin}
+                    className='bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none '
+                  >
+                  Save pin
+
+
+                  </button>
+
+              </div>
+
+            </div>
 
 
       </div>
--- sharewall_frontend/src/components/PinDetail.jsx
+++ sharewall_frontend/src/components/PinDetail.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { MdDownloadForOffline } from 'react-icons/md'
+import { Form, Link, useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
 
-const PinDeatail = () => {
+import { client, urlFor } from '../client'
+import MasonryLayout from './MasonryLayout'
+import { pinDetailQuery, PinDetailMorepinQuery, pinDetailMorePinQuery } from '../utils/data'
+import Spinner from './Spinner'
+
+
+const PinDeatail = ({user}) => {
+  const [pins, setPins] = useState(null)
+  const [pinDetail, setPinDetail] = useState(null)
+  const [comment, setComment] = useState('')
+  const [addingComment, setAddingComment] = useState(false)
+  const { pinId} = useParams();
+
+  
+
+  const fetchPinDetail = () =>{
+    let query = pinDetailQuery(pinId)
+
+    if(query){
+      client.fetch(query)
+      .then((data) =>{
+        setPinDetail(data[0]);
+
+        if(data[0]){
+          query = pinDetailMorePinQuery(data[0])
+
+          client.fetch(query)
+          .then((res) => setPins(res))
+        }
+      })
+    }
+
+
+  }
+
+  useEffect(() => {
+    fetchPinDetail()
+
+  }, [pinId])
+  
+
+  if(!pinDetail) return <Spinner message='Loading pin...'/>
+
   return (
-    <div>PinDeatail</div>
+    <div className='flex xl-flex-row flex-col m-auto bg-white' style={{maxwidth: '1500px', borderRadius: '32px'}}>
+      <div className='felx justify-center items-center md:items-start flex-initial'>
+        <img 
+          src={pinDetail?.image && urlFor(pinDetail.image).url()}
+          className = 'rounded-t-3xl rounded-b-lg'
+          alt='user-post'
+        />
+
+      </div>
+      <div className='w-full p-5 flex-1 xl:min-w-620'>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-2 items-center'>
+          <a
+                  href={`${pinDetail.image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                    <MdDownloadForOffline />
+                </a>
+          </div>
+          <a href={pinDetail.destination} target='blank' rel="noreferrer">
+          {pinDetail.destination}
+          </a>
+      </div>
+      <div>
+        <h1 className='text-4xl font-bold break-words mt-3'>
+          {pinDetail.title}
+        </h1>
+        <p className='mt-3'>{pinDetail.about}</p>
+      </div>
+      <Link to={`user-profile/${pinDetail.postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+                    <img
+                        className='w-8 h-8 rounded-full object-cover'
+                        src={pinDetail.postedBy?.image}
+                        alt='user-profile'
+                    />
+                    <p className='font-semibold capitalize'>{pinDetail.postedBy?.userName} </p>
+        </Link>
+
+      </div>
+    </div>
   )
 }
 
